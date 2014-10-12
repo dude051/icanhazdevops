b58a6659ec26dfaf6fe7c4b9bb416c7f6fe97cab
Justin Seubert <justin.seubert@rackspace.com>
2014-10-12 16:17:31 -0500

    Initial project commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'icanhazdevops'
+maintainer       'Justin Seubert'
+maintainer_email 'justin.seubert@rackspace.com'
+license          'All rights reserved'
+description      'Installs/Configures icanhazdevops'
+long_description 'Installs/Configures icanhazdevops'
+version          '0.1.0'
+
