1def6bdbe1301b3244f4519a643ad44d3582ca59
Justin Seubert <justin.seubert@rackspace.com>
2014-10-12 18:39:01 -0500

    COMPLETD PACKAGEZ INSTALL ADDD ATTRIBUTEZ FILE

---
+++ attributes/gem_installer.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: icanhazdevops
+# Attribute:: gem_installer
+#
+# Copyright (C) 2014 Justin Seubert
+#
+# All rights reserved - Do Not Redistribute
+#
+node.default['gem_installer']['gems'] = {
+    fog: nil
+}
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,19 @@
 # Cookbook Name:: icanhazdevops
 # Recipe:: default
 #
-# Copyright (C) 2014 YOUR_NAME
+# Copyright (C) 2014 Justin Seubert
 #
 # All rights reserved - Do Not Redistribute
 #
+%w(
+  nginx
+  vim
+  screen
+  python
+  ruby
+  gem_installer
+  curl
+  git
+).each do |cookbook|
+  include_recipe cookbook
+end
