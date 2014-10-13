dad14f7af843a2356a97cac898e97128ac48bbec
Justin Seubert <justin.seubert@rackspace.com>
2014-10-12 23:24:46 -0500

    VERSHUN BUMP

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'justin.seubert@rackspace.com'
 license          'All rights reserved'
 description      'Installs/Configures icanhazdevops'
 long_description 'Installs/Configures icanhazdevops'
-version          '0.1.0'
+version          '0.1.1'
 
 %w(
   nginx
