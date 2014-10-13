cde002ffa41c431d6b13c9a08999a24953998b13
Justin Seubert <justin.seubert@rackspace.com>
2014-10-12 22:19:18 -0500

    ADDD BACON

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: icanhazdevops
+# Attribute:: default
+#
+# Copyright (C) 2014 Justin Seubert
+#
+# All rights reserved - Do Not Redistribute
+#
+default['icanhazdevops']['bacon'] = true
