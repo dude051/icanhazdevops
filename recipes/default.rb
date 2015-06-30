6d12c9015c866e1cd7ac3c71708e5ba7fd9c9e82
Justin Seubert <dude051@gmail.com>
2015-06-30 12:59:18 -0500

    Fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,27 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 %w(
+  apt
   nginx
   vim
   icanhazdevops::nginx
 ).each do |cookbook|
   include_recipe cookbook
-end
-
-# Devel packages needed for fog install
-case node['platform']
-when 'redhat', 'centos'
-  lib_pkg = %w(
-    libxslt-devel
-    libxml2-devel
-  )
-when 'debian', 'ubuntu'
-  lib_pkg = %w(
-    libxslt-dev
-    libxml2-dev
-  )
-end
-
-lib_pkg.each do |pkg|
-  package pkg
 end
